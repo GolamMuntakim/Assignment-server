4e6e8d6c03880374992b7a619556332da5d1f964
Golam Muntakim <seams123456789@gmail.com>
2024-05-14 21:29:11 -0700

    jwt implementation done

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const cors = require('cors')
+const jwt = require('jsonwebtoken')
+const cookieParser = require('cookie-parser')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000
@@ -11,8 +13,27 @@ const corsOption = {
 }
 app.use(cors(corsOption))
 app.use(express.json())
+app.use(cookieParser())
 
-// const { MongoClient, ServerApiVersion } = require('mongodb');
+//middleware
+const verifyToken = (req,res,next)=>{
+  const token = req.cookies?.token
+  if(!token) return res.status(401).send({message:'unauthorized access'})
+      if(token){
+        jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decoded)=>{
+          if(err){
+            return res.status(401).send({message:'unauthorized access'})
+            // return
+          }
+          console.log(decoded)
+          req.user = decoded
+          next()
+        })
+      }
+     
+}
+
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ajfjwu7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -28,7 +49,37 @@ async function run() {
   try {
     const assignmentsCollection = client.db('b9-a11').collection('assignments')
     const submittedCollection = client.db('b9-a11').collection('submittedAssignments')
-    const marksCollection = client.db('b9-a11').collection('marks')
+    // const marksCollection = client.db('b9-a11').collection('marks')
+
+    //jwt generate
+    app.post('/jwt', async(req,res)=>{
+      const user = req.body
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET,{
+        expiresIn: '365d',
+      })
+      res.cookie('token' ,token,{
+        httpOnly:true,
+        secure:process.env.NODE_ENV ==='production',
+        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+      })
+      .send({success : true})
+    })
+
+    //clear cookie on logout
+    app.get('/logout',(req,res)=>{
+      res.clearCookie('token' ,{
+        httpOnly:true,
+        secure:process.env.NODE_ENV ==='production',
+        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+        maxAge:0,
+      })
+      .send({success : true})
+    })
+
+
+
+
+
      // get all assignments  data from db
      app.get('/assignments', async(req,res)=>{
         const result = await assignmentsCollection.find().toArray()
@@ -54,7 +105,7 @@ async function run() {
       res.send(result)
      })
      //delete assignments database 
-     app.delete('/deleteassignment/:email', async(req,res)=>{
+     app.delete('/deleteassignment/:email',verifyToken, async(req,res)=>{
       const email = req.params.email
       const query = {'maker.email' : email}
       const result = await assignmentsCollection.deleteOne(query)
@@ -76,8 +127,12 @@ async function run() {
      })
 
      //Get all submitted assignments
-     app.get('/submit/:email', async(req,res)=>{
+     app.get('/submit/:email',verifyToken, async(req,res)=>{
+      const tokenEmail = req.user.email
       const useremail = req.params.email
+      if(tokenEmail !== useremail){
+        return res.status(403).send({message:'forbidden access'})
+      }
       const query = {email : useremail}
       const result = await submittedCollection.find(query).toArray()
       res.send(result)
